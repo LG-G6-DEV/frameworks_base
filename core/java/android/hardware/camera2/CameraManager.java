47268d7aaabb535748e6c59ff1c71b08c9642537
Kyu <33372279+kyunal@users.noreply.github.com>
2019-10-14 20:23:47 +0200

    workaround for "phantom" camera (id 3) appearing in camera2
    this fix just skips the 3rd element when iterating over all devices to retrieve the id list,
    this allows portable cameras via otg to still be used while removing the problematic "phantom" camera

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -940,6 +940,7 @@ public final class CameraManager {
                 int idCount = 0;
                 for (int i = 0; i < mDeviceStatus.size(); i++) {
                     if(!exposeAuxCamera && (i == 2)) break;
+                    if(i == 3) continue;    // skip the 3rd id as this must be our phantom cam
                     int status = mDeviceStatus.valueAt(i);
                     if (status == ICameraServiceListener.STATUS_NOT_PRESENT ||
                             status == ICameraServiceListener.STATUS_ENUMERATING) continue;
@@ -949,6 +950,7 @@ public final class CameraManager {
                 idCount = 0;
                 for (int i = 0; i < mDeviceStatus.size(); i++) {
                     if(!exposeAuxCamera && (i == 2)) break;
+                    if(i == 3) continue;    // skip the 3rd id as this must be our phantom cam
                     int status = mDeviceStatus.valueAt(i);
                     if (status == ICameraServiceListener.STATUS_NOT_PRESENT ||
                             status == ICameraServiceListener.STATUS_ENUMERATING) continue;
